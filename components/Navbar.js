7011fdfd3401b7daa0b69f1873f0dbadc988801a
sureshgvl <86619863+sureshgvl@users.noreply.github.com>
2023-07-15 17:10:06 +0530

    first commit

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <header className=' bg-[#222222]  border-b-2 border-white flex justify-between mx-auto'> 
 <div className='flex '>
-  <img src='logo2new.png' className='object-contain w-44'/>
+  
   <div className=' items-center flex text-5xl font-serif  text-white space-x-5'>
      <h1 > ADITYA </h1>
      <div className=' text-end'><h1 className=' text-xl'> As Always</h1></div>
